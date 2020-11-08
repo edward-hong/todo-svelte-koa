5a7db4fbfe432e00bed0c9cd1732dd8b58ff78c8
Edward Hong <edward.hong527@gmail.com>
2020-11-09 01:34:33 +1300

    change user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,21 +1,15 @@
 const dynamoose = require('dynamoose')
-const crypto = require('crypto')
 
 const userSchema = new dynamoose.Schema(
   {
+    id: String,
     name: {
       type: String,
-      trim: true,
       required: true,
-      max: 32,
     },
     email: {
       type: String,
-      trim: true,
       required: true,
-      unique: true,
-      lowercase: true,
-      hashKey: true,
     },
     hashedPassword: {
       type: String,
@@ -23,41 +17,11 @@ const userSchema = new dynamoose.Schema(
     },
     salt: String,
     resetPasswordLink: {
-      data: String,
+      type: String,
       default: '',
     },
   },
   { timestamps: true },
 )
-
-// userSchema
-//   .virtual('password')
-//   .set(function (password) {
-//     this._password = password
-//     this.salt = this.makeSalt()
-//     this.hashedPassword = this.encryptPassword(password)
-//   })
-//   .get(function () {
-//     return this._password
-//   })
-
-userSchema.methods = {
-  authenticate: function (plainText) {
-    return this.encryptPassword(plainText) === this.hashedPassword
-  },
-
-  encryptPassword: function (password) {
-    if (!password) return ''
-    try {
-      return crypto.createHmac('sha1', this.salt).update(password).digest('hex')
-    } catch (err) {
-      return ''
-    }
-  },
-
-  makeSalt: function () {
-    return Math.round(new Date().valueOf() * Math.random()) + ''
-  },
-}
 
 module.exports = dynamoose.model('User', userSchema)

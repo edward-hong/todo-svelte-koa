040011297415ca32b1fd79e1d96b1c60d62cfc0f
Edward Hong <edward.hong527@gmail.com>
2020-12-30 22:23:43 +1300

    add Navbar

---
+++ client/src/Navbar.svelte
@@ -0,0 +1,23 @@
+<style>
+  #brand {
+    font-size: 1.5rem;
+    display: inline-block;
+    margin-left: 10px;
+  }
+</style>
+
+<div class="top-bar">
+  <div class="top-bar-left">
+    <a id="brand" href="/">Todo</a>
+  </div>
+  <div class="top-bar-right">
+    <ul class="menu">
+      <li>
+        <a href="/signup">Signup</a>
+      </li>
+      <li>
+        <a href="/signin">Signin</a>
+      </li>
+    </ul>
+  </div>
+</div>
